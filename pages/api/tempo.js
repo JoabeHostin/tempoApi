d10061175f5d6d911a6f11def0d0e9827b3a0364
JoabeHostin <105891888+JoabeHostin@users.noreply.github.com>
2022-07-07 14:09:41 -0300

    Update tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -6,15 +6,17 @@ async function tempo(request, response) {
 
     const dynamicDate = new Date();   
 
-    const cotationResponse = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL");
+    const cotationResponse = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL");
     const cotationResponseJson = await cotationResponse.json()
-    const cotation = cotationResponseJson;
+    const cotationUsd = cotationResponseJson.USDBRL.bid;
+    const cotationEur = cotationResponseJson.EURBRL.bid;
 
     
 
     response.json({
-        date: dynamicDate.toGMTString(),
-        cotation: cotation
+        Date: dynamicDate.toGMTString(),
+        USDBRL: cotationUsd,
+        EURBRL: cotationEur
     });
 
 }
