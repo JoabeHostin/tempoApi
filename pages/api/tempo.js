212bc4189c6239fb1727c01f6a5172c2da01f1dc
JoabeHostin <105891888+JoabeHostin@users.noreply.github.com>
2022-07-07 13:40:55 -0300

    Update tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,18 +1,17 @@
-//get ip
-
-function tempo(request, response) {
-
-//get location
 
 
-
+async function tempo(request, response) {
 
     const dynamicDate = new Date();   
-               
+    
+    const locationResponse = await fetch("https://api.bigdatacloud.net/data/ip-geolocation-with-confidence?ip=&localityLanguage=en&key=bdc_6ee4dcde331b482a99a2e453c3ba4987");
+    const locatioResponseJson = await locationResponse.json();
 
+    const location = locatioResponseJson.location.city;
 
     response.json({
-        date: dynamicDate.toGMTString() 
+        date: dynamicDate.toGMTString(),
+        location: location
     });
 
 }
